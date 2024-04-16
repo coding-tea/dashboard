0356dc0f66b6938eab633d862aa72d942230ad81
coding-tea <prgaming.ismail@gmail.com>
2024-04-16 18:30:16 +0100

    update layout for phone

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -80,7 +80,7 @@
     </nav>
 
     <aside id="logo-sidebar"
-        class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 "
+        class="fixed top-0 left-0 z-40 w-56 sm:w-64 h-screen pt-20 transition-transform bg-white border-r border-gray-200 sm:translate-x-0"
         aria-label="Sidebar">
         <div class="h-full px-3 pb-4 overflow-y-auto bg-white">
             <div class="space-y-2 font-medium flex flex-col h-full">
