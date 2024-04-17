fdf6ec276bd2943762fb73a428e376961ecd3279
coding-tea <prgaming.ismail@gmail.com>
2024-04-17 16:33:49 +0100

    2.Alect component

--- resources/views/pages/components.blade.php
+++ resources/views/pages/components.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
     @php
-        $class = \App\Enums\Alert::RED->value;
+        $class = \App\Enums\Alert::BLUE->value;
     @endphp
     <x-alert :class="$class">
         hello world! this is a normal message.
