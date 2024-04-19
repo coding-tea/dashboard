102ded89f47f741d393416b24115d4d515da85a7
coding-tea <prgaming.ismail@gmail.com>
2024-04-19 23:29:46 +0100

    set up new sidbar

--- resources/views/includes/css.blade.php
+++ resources/views/includes/css.blade.php
@@ -1,13 +1,7 @@
 {{-- @vite(['resources/css/app.css','resources/js/app.js']) --}}
 <script src="https://cdn.tailwindcss.com"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
-
-    body {
-        overflow-x: hidden;
-    }
 </style>
--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,2 +1,17 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.min.js" defer></script>
+<script>
+    const setup = () => {
+        return {
+            isSidebarOpen: false,
+            currentSidebarTab: null,
+            isSettingsPanelOpen: false,
+            isSubHeaderOpen: false,
+            watchScreen() {
+                if (window.innerWidth <= 1024) {
+                    this.isSidebarOpen = false
+                }
+            },
+        }
+    }
+</script>
