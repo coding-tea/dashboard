a733588b3a0e6236771f6df0e086d90c6025a0c9
coding-tea <prgaming.ismail@gmail.com>
2024-04-23 23:10:42 +0100

    update layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,23 +76,6 @@
             </div>
             <div class="flex flex-col flex-1">
                 <header class="relative flex items-center justify-between flex-shrink-0 p-4">
-                    <form action="#" class="flex-1">
-                        <!--  -->
-                    </form>
-                    <div class="items-center hidden ml-4 sm:flex">
-                        <button @click="isSettingsPanelOpen = true"
-                            class="p-2 mr-4 text-gray-400 bg-white rounded-lg shadow-md hover:text-gray-600 focus:outline-none focus:ring focus:ring-white focus:ring-offset-gray-100 focus:ring-offset-4">
-                            <span class="sr-only">Settings</span>
-                            <svg aria-hidden="true" class="w-6 h-6" xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                            </svg>
-                        </button>
-
-                    </div>
 
                     <!-- Mobile sub header button -->
                     <button @click="isSubHeaderOpen = !isSubHeaderOpen"
@@ -115,18 +98,6 @@
                         x-transition:leave-end="translate-y-full opacity-0" x-show="isSubHeaderOpen"
                         @click.away="isSubHeaderOpen = false"
                         class="absolute flex items-center justify-between p-2 bg-white rounded-md shadow-lg sm:hidden top-16 left-5 right-5">
-                        <!-- Seetings button -->
-                        <button @click="isSettingsPanelOpen = true; isSubHeaderOpen = false"
-                            class="p-2 text-gray-400 bg-white rounded-lg shadow-md hover:text-gray-600 focus:outline-none focus:ring focus:ring-white focus:ring-offset-gray-100 focus:ring-offset-4">
-                            <span class="sr-only">Settings</span>
-                            <svg aria-hidden="true" class="w-6 h-6" xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                            </svg>
-                        </button>
                         <!-- Messages button -->
                         <button
                             @click="isSidebarOpen = true; currentSidebarTab = 'messagesTab'; isSubHeaderOpen = false"
@@ -167,16 +138,6 @@
         <!-- Backdrop -->
         <div x-show="isSettingsPanelOpen" class="fixed inset-0 bg-black bg-opacity-50"
             @click="isSettingsPanelOpen = false" aria-hidden="true"></div>
-        <!-- Panel -->
-        <section x-transition:enter="transform transition-transform duration-300"
-            x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0"
-            x-transition:leave="transform transition-transform duration-300" x-transition:leave-start="translate-x-0"
-            x-transition:leave-end="translate-x-full" x-show="isSettingsPanelOpen"
-            class="fixed inset-y-0 right-0 w-64 bg-white border-l border-blue-100 rounded-l-3xl">
-            <div class="px-4 py-8">
-                <h2 class="text-lg font-semibold">Settings</h2>
-            </div>
-        </section>
 
 
 
