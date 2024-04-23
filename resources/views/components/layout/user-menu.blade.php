9dc23b10763be14b1f767ea03b07711a45c3c88f
coding-tea <prgaming.ismail@gmail.com>
2024-04-23 23:17:35 +0100

    update components names and restructure

--- resources/views/components/layout/user-menu.blade.php
+++ resources/views/components/layout/user-menu.blade.php
@@ -1,22 +1,22 @@
-<div class="relative flex items-center flex-shrink-0 p-2" x-data="{ isOpen: false } ">
+<div class="relative flex items-center flex-shrink-0 p-2" x-data="{ isOpen: false }">
     <button @click="isOpen = !isOpen; $nextTick(() => {isOpen ? $refs.userMenu.focus() : null})"
         class="transition-opacity rounded-lg opacity-80 hover:opacity-100 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-offset-white focus:ring-offset-2">
-        <img class="w-8 h-8 rounded-lg shadow-md"
+        <img class="w-10 h-10 rounded-lg shadow-md"
             src="{{ asset("assets/images/logo.png") }}"
             alt="dashboard logo" />
-        <span class="sr-only">User menu </span>
+        <span class="sr-only">User menu</span>
     </button>
     <div x-show="isOpen" @click.away="isOpen = false" @keydown.escape="isOpen = false"
         x-ref="userMenu" tabindex="-1"
         class="absolute w-48 py-1 mt-2 origin-bottom-left bg-white rounded-md shadow-lg left-10 bottom-14 focus:outline-none"
         role="menu" aria-orientation="vertical" aria-label="user menu">
         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            role="menuitem">Your Profile x</a>
+            role="menuitem">Your Profile</a>
 
         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            role="menuitem">Settings x</a>
+            role="menuitem">Settings</a>
 
         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            role="menuitem">Sign out x</a>
+            role="menuitem">Sign out</a>
     </div>
 </div>
